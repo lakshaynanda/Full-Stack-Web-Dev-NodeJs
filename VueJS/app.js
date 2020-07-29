f6770962ae0a2eb333db6ce8b7d86a31bc53cbee
lakshaynanda <lakshay.nanda08@gmail.com>
2020-07-29 19:30:16 +0530

    Computed Properties

--- VueJS/app.js
+++ VueJS/app.js
@@ -4,12 +4,14 @@ new Vue({
         name: '',
         username:'',
         title: 'Software Engineer',
-        age:'10',
+        age:'20',
         job: 'Web Development',
         count:0,
         isEven: true,
         x:0,
         y:0,
+        a:0,
+        b:0,
         tasks:[
             {name:'first task', priority:0},
             {name:'second task', priority:1},
@@ -42,6 +44,14 @@ new Vue({
         },
         logAge:function(){
             console.log('Age was entered');
-        }
+        }  
+    },
+    computed:{
+        addToA:function(){
+            return parseInt(this.age)+parseInt(this.a);
+        },
+        addToB:function(){
+            return parseInt(this.age)+parseInt(this.b);
+        },
     }
 });
