f24772a3211f396275c64710e11efabd1478e4df
lakshaynanda <lakshay.nanda08@gmail.com>
2020-08-12 01:04:44 +0530

    vue update

--- VueJS/Summary 6/app.js
+++ VueJS/Summary 6/app.js
@@ -2,15 +2,26 @@ new Vue({
     el:'#vue-app',
     data:{
         Name:"Lakshay",
-        Age:"20",
+        Age:"22",
         Job:"SDE",
         website:"https://lakshaynanda.github.io/resume/",
-        websiteTag:"<a href='https://lakshaynanda.github.io/resume/'>Lakshay Nanda Wesbite</a>"
+        websiteTag:"<a href='https://lakshaynanda.github.io/resume/'>Lakshay Nanda Wesbite</a>",
+        x:0,
+        y:0
     },
     methods:{
         greet: function(time){
-            
             return "Good "+ time +" "+this.Name;
+        },
+        add: function(inc){
+            this.Age+=inc;
+        },
+        sub: function(dec){
+            this.Age-=dec;
+        },
+        updateXY:function(event){
+            this.x=event.offsetX;
+            this.y=event.offsetY;
         }
     }
 });
