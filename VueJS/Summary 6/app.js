0ddb743b76396ff5acde821459e4c307c54d2b10
lakshaynanda <lakshay.nanda08@gmail.com>
2020-08-12 23:21:47 +0530

    Vue Playlist

--- VueJS/Summary 6/app.js
+++ VueJS/Summary 6/app.js
@@ -1,13 +1,15 @@
 new Vue({
     el:'#vue-app',
     data:{
-        Name:"Lakshay",
-        Age:"22",
+        Name:"",
+        Age:20,
         Job:"SDE",
         website:"https://lakshaynanda.github.io/resume/",
         websiteTag:"<a href='https://lakshaynanda.github.io/resume/'>Lakshay Nanda Wesbite</a>",
         x:0,
-        y:0
+        y:0,
+        a:0,
+        b:0,
     },
     methods:{
         greet: function(time){
@@ -22,6 +24,21 @@ new Vue({
         updateXY:function(event){
             this.x=event.offsetX;
             this.y=event.offsetY;
+        },
+        logName:function(){
+            console.log("You entered your name");
+        },
+        logAge: function(){
+            console.log("You entered your age");
+        },
+        
+    },
+    computed:{
+        addToA:function(){
+            return this.a+this.Age;
+        },
+        addToB:function(){
+            return this.b+this.Age;
         }
     }
 });
