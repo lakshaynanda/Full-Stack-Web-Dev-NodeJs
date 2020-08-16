b1558a0bc055162b03faa364babdd19a25690b8e
lakshaynanda <lakshay.nanda08@gmail.com>
2020-08-16 23:57:19 +0530

    Ninjas Page Components

---
+++ VueJS/WebPage/WebPage/src/App.vue
@@ -0,0 +1,29 @@
+<template>
+  <div>
+    <app-Header></app-Header>
+    <Ninjas></Ninjas>
+    <app-Footer></app-Footer>
+  </div>
+</template>
+
+<script>
+import Header from './components/Header.vue'
+import Footer from './components/Footer.vue'
+import Ninjas from './components/Ninjas.vue'
+export default {
+  components:{
+    'app-Header':Header,
+    'app-Footer':Footer,
+    'Ninjas':Ninjas
+  },
+  data () {
+    return {
+      
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
