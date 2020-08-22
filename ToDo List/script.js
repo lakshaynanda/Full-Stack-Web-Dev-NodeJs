d1d8cc1d243b0e54775bee7a9221d66b8b904c5c
lakshaynanda <lakshay.nanda08@gmail.com>
2020-08-23 03:32:03 +0530

    Todo list complete

--- ToDo List/script.js
+++ ToDo List/script.js
@@ -1,9 +1,17 @@
 let ulTasks=$('#ulTasks')
 let btnAdd=$('#btnAdd')
-let btnClear=$('#btnClear')
+let btnReset=$('#btnReset')
 let inpNewTask=$('#inpNewTask')
+let btnCleanup=$('#btnCleanup')
+let btnSort=$('#btnSort')
 
-
+function clearDone(){
+    $('#ulTasks .done').remove()
+    toggleInpButtons()
+}
+function sortTasks(){
+    $('#ulTasks .done').appendTo(ulTasks)
+}
 function addItem(){
     let listItem = $('<li>',{
         'class':'list-group-item',
@@ -14,16 +22,24 @@ function addItem(){
     })
     ulTasks.append(listItem)
     inpNewTask.val('')
+    toggleInpButtons()
 }
-
+function toggleInpButtons(){
+    btnReset.prop('disabled',inpNewTask.val()=='')
+    btnAdd.prop('disabled',inpNewTask.val()=='')
+    btnSort.prop('disabled',ulTasks.children().length < 1)
+    btnCleanup.prop('disabled',ulTasks.children().length < 1)
+}
 inpNewTask.keypress((e)=>{
     if(e.which==13){
         addItem()
     }
 })
-
+inpNewTask.on('input',toggleInpButtons)
 btnAdd.click(addItem)
-
-btnClear.click(()=>{
+btnCleanup.click(clearDone)
+btnSort.click(sortTasks)
+btnReset.click(()=>{
     inpNewTask.val('')
+    toggleInpButtons()
 })
