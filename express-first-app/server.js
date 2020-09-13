4b9a004c6be5e8737e7e2adabc0a8d0c5e29c279
lakshaynanda <lakshay.nanda08@gmail.com>
2020-09-13 17:23:54 +0530

    Query in Search Express

--- express-first-app/server.js
+++ express-first-app/server.js
@@ -3,10 +3,12 @@ const express = require('express')
 const app = express()
 
 app.get('/hello', (req,res) => {
-    res.send('Hello World')
+    let name = 'Guest'
+    if(req.query.user) { name = req.query.user }
+    res.send('Hello World ' + name)
 })
 
-app.use('/xyz',express.static(__dirname + '/public'))
+app.use('/xyz',express.static(__dirname + '/public')) //mounting public on /xyz
 
 app.listen(4321, () => {
     console.log('Server started on http://localhost:4321')
