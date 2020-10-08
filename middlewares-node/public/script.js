02a3ca24ede4cc2841c0f9a872e12f5a14fa3753
lakshaynanda <lakshay.nanda08@gmail.com>
2020-10-09 03:33:31 +0530

    ENCODER DECODER

---
+++ middlewares-node/public/script.js
@@ -0,0 +1,10 @@
+let inpCode = document.getElementById('inpCode')
+let btnEncode = document.getElementById('btnEncode')
+
+let code = document.getElementById('code')
+
+btnEncode.onclick = function() {
+    let data = inpCode.value
+    data = btoa(data)
+    code.value = data
+}
--- middlewares-node/server.js
+++ middlewares-node/server.js
@@ -2,36 +2,22 @@ const express = require('express')
 
 const app = express()
 
-function m1(req, res, next) {
-    console.log('Running middleware 1')
+app.use('/', express.static(__dirname + '/public'))
+
+function decodeQueryBase64(req, res, next) {
+    for (let q in req.query) {
+        let data = req.query[q]
+        data = new Buffer(data, 'base64').toString('ascii')
+        req.query[q] = data
+    }
     next()
 }
 
-
-function m2(req, res, next) {
-    console.log('Running middleware 2')
-    next()
-}
-
-function m3(req, res, next) {
-    console.log('Running middleware 3')
-    next()
-}
-
-function m4(req, res, next) {
-    console.log('Running middleware 4')
-    next()
-}
-
-function m5(req, res, next) {
-    console.log('Running middleware 5')
-    next()
-}
-
-app.get('/hello', m2, m3, (req, res) => {
-    res.send('Hello World')
+app.get('/eval', decodeQueryBase64, (req, res) => {
+    console.log(req.query)
+    res.send("=========Eval Result=============")
 })
 
-app.listen(4343, () => {
-    console.log('Server running at localhost:4343')
+app.listen(4545, () => {
+    console.log('Server Running at localhost:4545')
 })
