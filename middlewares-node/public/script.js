a1f08f6010c40db87fe1d15be85049f10b35ddd0
lakshaynanda <lakshay.nanda08@gmail.com>
2020-10-09 14:14:16 +0530

    Code Evaluator with Encryption

--- middlewares-node/public/script.js
+++ middlewares-node/public/script.js
@@ -1,10 +1,22 @@
 let inpCode = document.getElementById('inpCode')
 let btnEncode = document.getElementById('btnEncode')
-
+let btnEncrypt = document.getElementById('btnEncrypt')
 let code = document.getElementById('code')
 
 btnEncode.onclick = function() {
     let data = inpCode.value
     data = btoa(data)
     code.value = data
+}
+
+btnEncrypt.onclick = function() {
+    let data = code.value
+    data = encryptData(data)
+    code.value = data
+}
+
+function encryptData(str) {
+    return str.replace(/([a-z]+)|([A-Z]+)/g, function(match, chr) {
+        return chr ? match.toUpperCase() : match.toLowerCase();
+    });
 }
--- middlewares-node/server.js
+++ middlewares-node/server.js
@@ -13,9 +13,20 @@ function decodeQueryBase64(req, res, next) {
     next()
 }
 
-app.get('/eval', decodeQueryBase64, (req, res) => {
-    console.log(req.query)
-    res.send("=========Eval Result=============")
+function decryptQueryParams(req, res, next) {
+    for (let q in req.query) {
+        let data = req.query[q]
+        data = data.replace(/([a-z]+)|([A-Z]+)/g, function(match, chr) {
+            return chr ? match.toUpperCase() : match.toLowerCase();
+        })
+        req.query[q] = data
+    }
+    next()
+}
+
+app.get('/eval', decryptQueryParams, decodeQueryBase64, (req, res) => {
+    // console.log(req.query)
+    res.send(req.query.code)
 })
 
 app.listen(4545, () => {
